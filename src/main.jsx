79cd585c587bc147e48bbc7d40891801276ca390
Josh Wang <Joshw9012@gmail.com>
2023-12-20 23:09:46 +1100

    final version 20-12

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,10 @@ import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ErrorBoundary
-      FallbackComponent={ErrorFallback}
-      onReset={() => window.location.replace("/")}
-    >
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+  <ErrorBoundary
+    FallbackComponent={ErrorFallback}
+    onReset={() => window.location.replace("/")}
+  >
+    <App />
+  </ErrorBoundary>
 );
